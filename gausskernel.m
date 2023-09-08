8307bb3a93fc29779ae975f65dad560746b6a598
flipe rodrigues <filipe.rodrigues@neuro.fchampalimaud.org>
2023-09-08 14:32:40 +0100

    Update gausskernel.m
    centered kernel such that it has the exact same number of bins to the left and right of 0

--- gausskernel.m
+++ gausskernel.m
@@ -9,9 +9,10 @@ function k = gausskernel(varargin)
     p.parse(varargin{:});   
     
     k = p.Results;
-    k.nbins = round(k.sig / k.binwidth * 10);
+    k.nbins = 2 * round(k.sig / k.binwidth * 8.5 / 2);
     k.paddx = [-1,1] / 2 * k.nbins * k.binwidth;
-    k.bins = -k.nbins / 2 + 1 : k.nbins / 2;
+    k.bins = -k.nbins / 2 + 1 : k.nbins / 2 - 1;
+    k.nbins = numel(k.bins);
     k.x = k.bins * k.binwidth;
     k.pdf = normpdf(k.x,0,k.sig);
     k.pdf = k.pdf / sum(k.pdf);
